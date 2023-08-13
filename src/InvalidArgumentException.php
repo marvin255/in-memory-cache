106582fa7776ff933a9bff9b485dfa1c073085a1
marvin255 <postbox433@gmail.com>
2023-08-13 10:22:14 +0200

    Fix InvalidArgumentException

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Marvin255\InMemoryCache;
 
+/**
+ * Specific cache related exception.
+ *
+ * @internal
+ */
 final class InvalidArgumentException extends \Exception implements \Psr\SimpleCache\InvalidArgumentException
 {
 }
